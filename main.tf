a586dab3d66c63058228781d3a250816e4a1d318
Jelena Kutalovskaja <jelena.kutalovskaja@gmail.com>
2018-06-06 23:29:05 +0300

    Terrafor Auto Scaling Group Setup

--- main.tf
+++ main.tf
@@ -8,11 +8,13 @@ variable "server_port" {
   default = 8080
 }
 
+data "aws_availability_zones" "all" {}
 
-resource "aws_instance" "example" {
-  ami = "ami-40d28157"
+resource "aws_launch_configuration" "example" {
+  # ami = "ami-40d28157"
+  image_id = "ami-40d28157"
   instance_type = "t2.micro"
-  vpc_security_group_ids = ["${aws_security_group.instance.id}"]
+  security_groups = ["${aws_security_group.instance.id}"]
 
   user_data = <<-EOF
               #!/bin/bash
@@ -20,9 +22,10 @@ resource "aws_instance" "example" {
               nohup busybox httpd -f -p "${var.server_port}" &
               EOF
 
-  tags {
-    Name = "terraform-example"
+  lifecycle {
+    create_before_destroy = true
   }
+
 }
 
 
@@ -35,8 +38,71 @@ resource "aws_security_group" "instance" {
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
-output "public_ip" {
-  value = "${aws_instance.example.public_ip}"
+resource "aws_security_group" "elb" {
+  name = "terraform-example-elb"
+
+  ingress {
+    from_port = 80
+    protocol = "tcp"
+    to_port = 80
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    protocol = "-1"
+    to_port = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+}
+
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = "${aws_launch_configuration.example.id}"
+  availability_zones = ["${data.aws_availability_zones.all.names}"]
+
+  load_balancers = ["${aws_elb.example.name}"]
+  health_check_type = "ELB"
+
+  max_size = 2
+  min_size = 10
+
+  tag {
+    key = "Name"
+    value = "terraform-asg-example"
+    propagate_at_launch = true
+  }
+}
+
+
+resource "aws_elb" "example" {
+  name = "terraform-asg-example"
+  availability_zones = ["${data.aws_availability_zones.all.names}"]
+  security_groups = ["${aws_security_group.elb.id}"]
+
+  "listener" {
+    instance_port = "${var.server_port}"
+    instance_protocol = "http"
+    lb_port = 80
+    lb_protocol = "http"
+  }
+
+  health_check {
+    healthy_threshold = 2
+    interval = 30
+    target = "HTTP:${var.server_port}/"
+    timeout = 3
+    unhealthy_threshold = 2
+  }
+
+}
+
+
+output "elb_dns_name" {
+  value = "${aws_elb.example.dns_name}"
 }
